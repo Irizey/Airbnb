945a5da2a31eab40a77160f4c2db8487ace99afd
Andrei Irizey <forgeofirizey@gmail.com>
2025-01-12 18:32:34 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/header/Header.jsx";
+import Main from "./components/main/Main.jsx";
+import Footer from "./components/footer/Footer.jsx";
+function App() {
+
+
+  return (
+    <>
+      <div>
+          <Header/>
+          <Main/>
+          <Footer/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import './style.scss';
+function Header() {
+    return (
+        <>
+          <header className='header'>HEADER</header>
+        </>
+    )
+}
+
+export default Header
