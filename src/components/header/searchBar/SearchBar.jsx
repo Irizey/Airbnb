c0831a83b20673f4db222fdc7e5b6a77611c0162
Andrei Irizey <forgeofirizey@gmail.com>
2025-01-13 13:14:34 +0300

    header

--- src/components/header/searchBar/SearchBar.jsx
+++ src/components/header/searchBar/SearchBar.jsx
@@ -1,9 +1,18 @@
 import './style.scss';
+import searchIcon from './search.svg'
 function searchBar() {
     return (
         <>
             <div className="searchBar">
-                DV
+                <div className="searchBar__btn-wrapper">
+                    <button className="searchBar__btn">Anywhere</button>
+                    <button className="searchBar__btn">Any week</button>
+                    <button className="searchBar__btn">Add quests</button>
+                </div>
+
+                <button className="searchBar__btn-search">
+                    <img src={searchIcon} alt="icon searche button"/>
+                </button>
             </div>
         </>
     )
